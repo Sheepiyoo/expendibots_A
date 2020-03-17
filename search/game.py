435f6ffdc8115fe859896d10cd9d45a901ca5205
Sheepiyoo <yeohzx99@gmail.com>
2020-03-17 20:38:14 +1100

    Changed to sequential program

--- search/game.py
+++ search/game.py
@@ -1,54 +1,54 @@
-class Board:
-    def __init__(self, initial_pos):
-        self.board_dict = initial_pos
-        self.update_grid()
-        self.board_size = 8
+# Converts from (x,y) as keys to "white", "black" as keys
+# {(x, y): "wn"} --> {"white": [n, x, y]}
 
-    def update_player(self):
-        player_format = {"white":[], "black": []}
-        for coordinate in self.grid_dict.keys():
-            x, y = coordinate
-            player, n = self.grid_dict[coordinate]
+def get_token_format(grid_dict):
+    token_format = {"white":[], "black": []}
+    for coordinate in grid_dict.keys():
+        x, y = coordinate
+        player, n = grid_dict[coordinate]
 
-            if player == 'w':
-                player_format["white"].append([int(n), x, y])
-            elif player == 'b':
-                player_format["black"].append([int(n), x, y])
+        if player == 'w':
+            token_format["white"].append([int(n), x, y])
+        elif player == 'b':
+            token_format["black"].append([int(n), x, y])
 
-        self.board_dict = player_format
-            
+    return token_format
 
-    def update_grid(self):
-        grid_format = {}
-        #Convert dictionary with players as keys into dictionary with coordinates as keys
-        for player in self.board_dict.keys():
-            for stack in self.board_dict[player]:
-                grid_format[(stack[1], stack[2])] = ''.join([player[0], str(stack[0])])
-        
-        self.grid_dict = grid_format
+# Converts from "white", "black" as keys to (x,y) as keys 
+# {"white": [n, x, y]} --> {(x, y): "wn"}       
+def get_grid_format(board_dict):
+    grid_format = {}
+    for player in board_dict.keys():
+        for stack in board_dict[player]:
+            grid_format[(stack[1], stack[2])] = ''.join([player[0], str(stack[0])])
+    
+    return grid_format
 
-    def move(self, n, x, y):
-        pass
+def move(n, x, y):
+    pass
 
-    def boom(self, x, y):
-        self.boom_recursive(x, y)
-        self.update_player()
-        
-    def boom_recursive(self, x, y):
-        #Check bounds
-        if not (0 <= x < self.board_size and 0 <= y < self.board_size):
-            return
-        
-        #If a token is present, explode!        
-        if (x,y) in self.grid_dict.keys():
-            del(self.grid_dict[(x,y)])
-            
-            #Debug line
-            print("removed token at" , x, y)
+# Preprocessing for boom
+def boom(x, y, board_dict):
+    grid_format = get_grid_format(board_dict)
+    boom_recursive(x, y, grid_format)
+    return get_token_format(grid_format)
 
-            #Recursive explosion
-            for i in range(-1,2):
-                for j in range(-1, 2):
-                    self.boom(x+i, y+j)
 
+def boom_recursive(x, y, grid_format):
+    #Check bounds
+    if not (0 <= x < 8 and 0 <= y < 8):
         return
+    
+    #If a token is present, explode!        
+    if (x, y) in grid_format.keys():
+        del(grid_format[(x,y)])
+        
+        #Debug line
+        print("removed token at" , x, y)
+
+        #Recursive explosion
+        for i in range(-1,2):
+            for j in range(-1, 2):
+                boom_recursive(x+i, y+j, grid_format)
+
+    return
