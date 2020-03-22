3369993541a57bbe2183ba7f9f568761d5ae5901
Sheepiyoo <zhongxien.yeoh@gmail.com>
2020-03-22 23:45:50 +1100

    BIG BRAINZZZZZZ

---
+++ search/big_brains.py
@@ -0,0 +1,31 @@
+# Node representation
+class Node:
+    def __init__(self, board_dict, path_cost, heuristic, action, parent):
+        self.state = board_dict
+        self.path_cost = path_cost
+        self.heuristic = heuristic
+        self.action = action
+        self.parent = parent
+        self.children = []
+
+    def __str__(self):
+        return str("""
+                    State: {}
+                    Path Cost: {}
+                    Heuristic: {}
+                    Action: {}
+                    Parent: {}
+                    Children: {}
+                    """.format(str(self.state), str(self.path_cost), str(self.heuristic), str(self.action), hex(id(self.parent)), str(self.children)))
+
+# Checks a node - True if no black token remains
+def goal_test(node):
+    # Need to implement token counter
+
+    return len(node.board_dict['black']) == 0
+
+# Implement heuristic function (Currently h(n) = number of black tokens) 
+def heuristic(node):
+    # Need to implement token counter
+
+    return len(node.board_dict['black'])
