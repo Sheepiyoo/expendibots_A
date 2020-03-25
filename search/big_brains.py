d69fe6a0c106cbd32f32ab86b43d7a19f779aa87
Emily Zhao <emilyzhao37@gmail.com>
2020-03-25 19:05:21 +1100

    Update big_brains.py

--- search/big_brains.py
+++ search/big_brains.py
@@ -33,3 +33,37 @@ def heuristic(node):
 # Implement A-star search
 def search(initial_state):
     pass
+
+def is_white(colour_n):
+    player, n = colour_n
+    if player == "w":
+        return True
+    return False
+
+def possible_positions(x, y, n):
+    positions = []
+    if y+n < 9:
+        positions.append((x, y+n))
+    if x+n < 9:
+        positions.append((x+n, y))
+    if (x-n) > 0:
+        positions.append((x-n, y))
+    if y-n > 0:
+        positions.append((x, y-n))
+    return positions
+
+
+def get_possible_moves(stack, board):
+    grid_board = get_grid_format(board)
+    possible_moves = []
+    x_pos = stack[1]
+    y_pos = stack[2]
+    possible_moves.append(["boom", (x_pos, y_pos), stack[0]])
+    for n in range(1, stack[0]):
+        for (x, y) in possible_positions(x_pos, y_pos, n):
+            if is_white(grid_board[(x, y)]):
+                possible_moves.append(["stack", x, y])
+            else:
+                for i in range(1, n):
+                    possible_moves.append(["move", (x, y), i])
+    return possible_moves
